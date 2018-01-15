3c8634b814a5d61a4f8c7e0926d04cf47cf6fcca
Sandy Zhang <sandy@flamingo.io>
2018-01-15 16:31:49 +1100

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## React and Redux Sagas Demo
+
+## Basic over using [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/index-sagas.js
@@ -0,0 +1,13 @@
+
+import SignupSaga from './signup/sagas'
+import LoginSaga from './login/sagas'
+import WidgetSaga from './widgets/sagas'
+
+
+export default function* IndexSaga () {  
+  yield [
+    SignupSaga(),
+    LoginSaga(),
+    WidgetSaga(),
+  ]
+}
